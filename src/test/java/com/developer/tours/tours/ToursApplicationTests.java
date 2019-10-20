f690cec0967184549fbf63892497fad076350f98
venkata <venkata@venkatas-MacBook-Pro.local>
2019-10-20 08:22:45 -0700

    Add tours application files

---
+++ src/test/java/com/developer/tours/tours/ToursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.tours.tours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
